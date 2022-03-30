785d47d3babc969efc64e6008e36cc247d811b8a
carloshvidigal <82422474+carloshvidigal@users.noreply.github.com>
2022-03-30 16:34:20 -0300

    Create aula11 (ex007js, ex008js and ex009.html)

---
+++ aula11/ex007.js
@@ -0,0 +1,7 @@
+var velocidade = 62
+    if(velocidade > 60){
+        console.log('Você ultrapassou a velocidade máxima permitida e foi MULTADO!')
+    }
+
+console.log(`A velocidade do seu carro é ${velocidade} km/h`)
+console.log(`Dirija sempre usando o cinto de segurança`)
---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}\n`)
+if (pais =='Brasil'){
+    console.log("Brasileiro\n")
+}else {
+    console.log("Estrangeiro\n")
+}
