d27d1d84d86dec886c55be1ad4cdcc6f772ea41e
carloshvidigal <82422474+carloshvidigal@users.noreply.github.com>
2022-04-05 19:09:08 -0300

    Create ex016 and script.js

--- aula14_exercicio_2/script.js
+++ aula14_exercicio_2/script.js
@@ -1,37 +1,12 @@
-function contar(){
-    let ini = document.getElementById('txti')
-    let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
-    let res = document.getElementById('result')
+function tabuada(){
+    let num = document.getElementById("txtn")
+    let tab = document.getElementById("seltab")
 
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        window.alert("[ERRO] Faltam dados! ")
-        res.innerHTML = 'Impossível contar'
+    if (num.value.length == 0){
+        window.alert("Por favor, digite um número!")
     } else {
-        res.innerHTML = "Contando: <br>"
-        let i = Number(ini.value) //transforma a String recebida pelo ini em número
-        let f = Number(fim.value)
-        let p = Number(passo.value)
-        if (p <= 0){
-            window.alert("Passo inválido! Considerando Passo = 1.")
-            p = 1
-        }
-
-       if (i < f){       
-           // contagem crescente
-         for(let c = i; c <= f; c += p){
-            res.innerHTML += `${c} \u{1F449}`
-            }
-        }else{
-            // contagem decrescente
-         for(let c = i; c >= f; c -= p){
-            res.innerHTML += `${c} \u{1F449}`
-            }   
-         }
-         res.innerHTML += `\u{1F3C1}` 
-        }
-
-       }
-        
-    
-
+        let n = Number(num.value)
+        let c = 1
+        while (c <= 30)
+    }
+}
